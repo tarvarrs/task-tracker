e5c33c6dffeaec99f76df7b3d85a5fe36d2ea3a6
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-12-10 11:20:43 +0300

    add some APIs, change backend port

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const sqlite3 = require('sqlite3') .verbose();
 
 dotenv.config();
 
@@ -9,11 +10,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+const db = new sqlite3.Database('../task-tracker.db');
+
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-const PORT = process.env.PORT || 5000;
+// API for getting all tasks
+app.get('/tasks', (req, res) => {
+  db.all('SELECT * FROM tasks', (err, rows) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.json(rows);
+  });
+});
+
+// API for getting task by id
+app.get('/tasks/:id', (req, res) => {
+  db.get('SELECT * FROM tasks WHERE id = ?', req.params.id, (err, row) => {
+    if (err) {
+      console.message(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+    if (!row) {
+      return res.status(404).send('Task not found');
+    }
+    res.json(row);
+  });
+});
+
+// API listener
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
