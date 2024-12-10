1d55e38960f52275eca6ecdbe45d8f2d4f4b69fc
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2024-12-10 20:25:59 +0300

    add CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,90 @@ app.get('/tasks/:id', (req, res) => {
   });
 });
 
+// API for task status update
+app.put('/tasks/:id/status', (req, res) => {
+  const taskId = req.params.id;
+  const { status } = req.body;
+
+  if (!status) {
+    return res.status(400).json({ message: 'Status is required' });
+  }
+
+  const validStatuses = ['В ожидании', 'В процессе', 'Выполнено'];
+  if (!validStatuses.includes(status)) {
+    return res.status(400).json({ message: 'Invalid status' });
+  }
+
+  const sql = 'UPDATE tasks SET status = ? WHERE id = ?';
+  db.run(sql, [status, taskId], function (err) {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+    if (this.changes === 0) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.status(200).json({ message: 'Task status updated successfully' });
+  });
+});
+
+// API for task delete
+app.delete('/tasks/:id', (req, res) => {
+  const taskId = req.params.id;
+
+  const sql = 'DELETE FROM tasks WHERE id = ?';
+  db.run(sql, [taskId], function (err) {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+    if (this.changes === 0) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.status(200).json({ message: 'Task deleted successfully' });
+  });
+});
+
+// API for task create
+app.post('/tasks', (req, res) => {
+  const { task_name, description, date_end, priority } = req.body;
+
+  if (!task_name || !priority) {
+    return res.status(400).json({ message: 'Task name and priority are required' });
+  }
+
+  const priorityMap = { '!': 1, '!!': 2, '!!!': 3 };
+  const priorityValue = priorityMap[priority];
+
+  if (!priorityValue) {
+    return res.status(400).json({ message: 'Invalid priority value' });
+  }
+
+  db.get('SELECT MAX(id) AS maxId FROM tasks', (err, row) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    const newId = (row?.maxId || 0) + 1;
+
+    const currentDate = new Date().toISOString().slice(0, 10);
+
+    const sql = `
+      INSERT INTO tasks (id, task_name, description, date_create, date_update, date_end, status, priority)
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+    db.run(sql, [newId, task_name, description || '', currentDate, currentDate, date_end || null, 'В ожидании', priorityValue], function (err) {
+      if (err) {
+        console.error(err.message);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      res.status(201).json({ message: 'Task added successfully', taskId: newId });
+    });
+  });
+});
+
 // API listener
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,112 +1,321 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]); // Все задачи
+  const [isLoading, setIsLoading] = useState(true); // Состояние загрузки
+  const [searchTaskId, setSearchTaskId] = useState(''); // ID для поиска
+  const [searchResult, setSearchResult] = useState(null); // Результат поиска
+  const [formData, setFormData] = useState({
+    task_name: '',
+    description: '',
+    date_end: '',
+    priority: '!',
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:4000/tasks')
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching tasks:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleSearch = () => {
+    if (!searchTaskId.trim()) {
+      setSearchResult(null);
+      return;
+    }
+
+    fetch(`http://localhost:4000/tasks/${searchTaskId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Task not found');
+        }
+      return response.json();
+      })
+      .then((task) => {
+          setSearchResult(task);
+        })
+      .catch((error) => {
+        console.warn(error.message);
+        alert('Задача с указанным ID не найдена');
+        setSearchResult(null);
+      });
+  }
+
+  const handleStatusChange = (taskId, newStatus) => {
+    fetch(`http://localhost:4000/tasks/${taskId}/status`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ status: newStatus }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to update task status');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.message);
+        setTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === taskId ? { ...task, status: newStatus} : task
+      ));
+      })
+      .catch((error) => {
+        console.log('Error updating task status:', error);
+        alert('Не удалось обновить статус задачи');
+      });
+  };
+
+  const handleDeleteTask = (taskId) => {
+    if (!window.confirm('Вы точно хотите удалить эту задачу? Это действие безвозвратно')) {
+      return;
+    }
+    fetch(`http://localhost:4000/tasks/${taskId}`, {
+      method: 'DELETE',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to delete task');
+        }
+      return response.json();
+      })
+      .then((data) => {
+        console.log(data.message);
+        setTasks((prevTask) => prevTasks.filter((task) => task.id !== taskId));
+      })
+      .catch((error) => {
+        console.error('Error deleting task:', error);
+        alert('Не удалось удалить задачу');
+      });
+  }
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+
+    fetch('http://localhost:4000/tasks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to add task');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data.message);
+        setTasks((prevTasks) => [
+          ...prevTasks,
+          {
+            id: data.taskId,
+            task_name: formData.task_name,
+            description: formData.description,
+            date_create: new Date().toISOString().slice(0, 10),
+            date_update: new Date().toISOString().slice(0, 10),
+            date_end: formData.date_end,
+            status: 'В ожидании',
+            priority: formData.priority,
+          },
+        ]);
+        setFormData({
+          task_name: '',
+          description: '',
+          date_end: '',
+          priority: '!',
+        });
+      })
+      .catch((error) => {
+        console.error('Error adding task:', error);
+        alert('Не удалось добавить задачу');
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <>
-    <div class="task-tracker">
-  <header class="header">
-    <button class="add-task-btn">+</button>
-    <span class="sort-label">Сортировать по</span>
-  </header>
-
-  <div class="task-card">
-    <div class="task-info">
-      <div class="task-header">
-        <p class="task-id">ID 001</p>
-      </div>
-      <div class="task-content">
-        <div class="task-left">
-          <p class="task-name"><strong>!!</strong>Задача 1</p>
-          <p class="task-description">Описание задачи 1</p>
-        </div>
-        <div class="task-right">
-          <p class="task-deadline">Дедлайн: 15-12-2024</p>
-          <div class="task-status">
-            <label for="status-1">Статус:</label>
-            <select id="status-1">
-              <option>В ожидании</option>
-              <option>В процессе</option>
-              <option>Выполнено</option>
+    <div className="task-tracker">
+      <header className="header">
+        <form className='add-task-form' onSubmit={handleAddTask}>
+          <div>
+            <label htmlFor='task_name'>Название задачи: </label>
+            <input
+              type='text'
+              id='task_name'
+              name='task_name'
+              value={formData.task_name}
+              onChange={handleInputChange}
+              required
+              />
+          </div>
+          <div>
+            <label htmlFor='description'>Описание: </label>
+            <textarea
+              id='description'
+              name='description'
+              value={formData.description}
+              onChange={handleInputChange}
+              />
+          </div>
+          <div>
+            <label htmlFor='date_end'>Срок выполнения: </label>
+            <input
+              type='date'
+              id='date_end'
+              name='date_end'
+              value={formData.date_end}
+              onChange={handleInputChange}
+              />
+          </div>
+          <div>
+            <label htmlFor='priority'>Приоритет: </label>
+            <select
+              id='priority'
+              name='priority'
+              value={formData.priority}
+              onChange={handleInputChange}
+              >
+                <option value='!'>!</option>
+                <option value='!!'>!!</option>
+                <option value='!!!'>!!!</option>
+              </select>
+          </div>
+          <button type='submit'>Добавить задачу</button>
+        </form>
+        <div className='header-second-line'>
+          <div className='search-task'>
+            <input 
+              type="text" 
+              name="task_id_input" 
+              placeholder='Введите ID задачи' 
+              value={searchTaskId}
+              onChange={(e) => setSearchTaskId(e.target.value)}
+              />
+            <button className='search-btn' onClick={handleSearch}>Поиск</button>
+          </div>
+          <div className="sort-by">
+            <label htmlFor="sort-1">Сортировать по </label>
+            <select id="sort-1">
+              <option>ID</option>
+              <option>дате</option>
+              <option>статусу</option>
+              <option>приоритету</option>
             </select>
           </div>
+          <span className="sort-label">Фильтр</span>
         </div>
-      </div>
-    </div>
-    <div class="task-dates">
-      <p><strong>Добавлено:</strong></p>
-      <p>10-09-2024</p>
-      <p><strong>Обновлено:</strong></p>
-      <p>11-09-2024</p>
-    </div>
-  </div>
-  <div class="task-card">
-    <div class="task-info">
-      <div class="task-header">
-        <p class="task-id">ID 001</p>
-      </div>
-      <div class="task-content">
-        <div class="task-left">
-          <p class="task-name"><strong>!!</strong>Задача 1</p>
-          <p class="task-description">Описание задачи 1</p>
-        </div>
-        <div class="task-right">
-          <p class="task-deadline">Дедлайн: 15-12-2024</p>
-          <div class="task-status">
-            <label for="status-1">Статус:</label>
-            <select id="status-1">
-              <option>В ожидании</option>
-              <option>В процессе</option>
-              <option>Выполнено</option>
-            </select>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="task-dates">
-      <p><strong>Добавлено:</strong></p>
-      <p>10-09-2024</p>
-      <p><strong>Обновлено:</strong></p>
-      <p>11-09-2024</p>
-    </div>
-  </div>
-  <div class="task-card">
-    <div class="task-info">
-      <div class="task-header">
-        <p class="task-id">ID 001</p>
-      </div>
-      <div class="task-content">
-        <div class="task-left">
-          <p class="task-name"><strong>!!</strong>Задача 1</p>
-          <p class="task-description">Описание задачи 1</p>
-        </div>
-        <div class="task-right">
-          <p class="task-deadline">Дедлайн: 15-12-2024</p>
-          <div class="task-status">
-            <label for="status-1">Статус:</label>
-            <select id="status-1">
-              <option>В ожидании</option>
-              <option>В процессе</option>
-              <option>Выполнено</option>
-            </select>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="task-dates">
-      <p><strong>Добавлено:</strong></p>
-      <p>10-09-2024</p>
-      <p><strong>Обновлено:</strong></p>
-      <p>11-09-2024</p>
-    </div>
-  </div>
+      </header>
 
-</div>
-
-    </>
-  )
+      <div className="task-list">
+        {isLoading ? (
+          <p>Загрузка задач...</p>
+        ) : searchResult ? (
+          <div className="task-card">
+            <div className="task-info">
+              <div className="task-header">
+                <p className="task-id">ID {searchResult.id}</p>
+              </div>
+              <div className="task-content">
+                <div className="task-left">
+                  <p className="task-name">
+                    <strong>{searchResult.priority}</strong> {searchResult.task_name}
+                  </p>
+                  <p className="task-description">{searchResult.description}</p>
+                </div>
+                <div className="task-right">
+                  <p className="task-deadline">Дедлайн: {searchResult.date_end}</p>
+                  <div className="task-status">
+                    <label htmlFor={`status-${searchResult.id}`}>Статус: </label>
+                    <select
+                      id={`status-${searchResult.id}`}
+                      // defaultValue={searchResult.status}
+                      value={searchResult.status}
+                      onChange={(e) => handleStatusChange(searchResult.id, e.target.value)}
+                    >
+                      <option>В ожидании</option>
+                      <option>В процессе</option>
+                      <option>Выполнено</option>
+                    </select>
+                  </div>
+                  <button 
+                    className='delete-task-btn'
+                    onClick={() => handleDeleteTask(searchResult.id)}
+                    >Удалить</button>
+                </div>
+              </div>
+            </div>
+            <div className="task-dates">
+              <p>
+                <strong>Добавлено:</strong> {searchResult.date_create}
+              </p>
+              <p>
+                <strong>Обновлено:</strong> {searchResult.date_update}
+              </p>
+            </div>
+          </div>
+        ) : (
+          tasks.map((task) => (
+            <div key={task.id} className="task-card">
+              <div className="task-info">
+                <div className="task-header">
+                  <p className="task-id">ID {task.id}</p>
+                </div>
+                <div className="task-content">
+                  <div className="task-left">
+                    <p className="task-name">
+                      <strong>{task.priority}</strong> {task.task_name}
+                    </p>
+                    <p className="task-description">{task.description}</p>
+                  </div>
+                  <div className="task-right">
+                    <p className="task-deadline">Дедлайн: {task.date_end}</p>
+                    <div className="task-status">
+                      <label htmlFor={`status-${task.id}`}>Статус: </label>
+                      <select 
+                        id={`status-${task.id}`} 
+                        // defaultValue={task.status}
+                        value={task.status}
+                        onChange={(e) => handleStatusChange(task.id, e.target.value)}
+                        >
+                        <option>В ожидании</option>
+                        <option>В процессе</option>
+                        <option>Выполнено</option>
+                      </select>
+                    </div>
+                    <button 
+                      className="delete-task-btn"
+                      onClick={() => handleDeleteTask(task.id)}
+                      >Удалить</button>
+                  </div>
+                </div>
+              </div>
+              <div className="task-dates">
+                <p>
+                  <strong>Добавлено:</strong> {task.date_create}
+                </p>
+                <p>
+                  <strong>Обновлено:</strong> {task.date_update}
+                </p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
